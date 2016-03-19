98e2a6c1b8e7d25c5e6b64cacc4603faa22e02bf
CoveyWang <ww8492032@sohu.com>
2016-03-19 16:54:56 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Covey on 2016/3/19.
+ */
+public class Solution {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
